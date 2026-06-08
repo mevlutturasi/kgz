124d768f7a8dd431759eecf7206b5440c3b26271
mevlutturasi <mevlut.tura_si@hotmail.com>
2026-06-09 02:24:58 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813002",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093829617.jpg?v=1273256375"
     },
     "1813005": {
@@ -12,7 +12,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813005",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093833431.jpg?v=989457907"
     },
     "1813006": {
@@ -20,7 +20,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813006",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093837986.jpg?v=779061741"
     },
     "1813007": {
@@ -28,7 +28,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813007",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093832060.jpg?v=293487366"
     },
     "1813008": {
@@ -36,7 +36,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813008",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093727561.jpg?v=148479953"
     },
     "1813013": {
@@ -44,7 +44,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813013",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093731757.jpg?v=1936180538"
     },
     "1813014": {
@@ -52,7 +52,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813014",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093834855.jpg?v=768914896"
     },
     "1813016": {
@@ -60,7 +60,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813016",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093729284.jpg?v=2026679549"
     },
     "1813017": {
@@ -68,7 +68,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813017",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093748732.jpg?v=1040778948"
     },
     "1813018": {
@@ -76,7 +76,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813018",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093836280.jpg?v=193039431"
     },
     "1813019": {
@@ -84,7 +84,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813019",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093721443.jpg?v=242068346"
     },
     "1813020": {
@@ -92,7 +92,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813020",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093819919.jpg?v=1134381968"
     },
     "1813022": {
@@ -100,7 +100,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813022",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093840384.jpg?v=639833563"
     },
     "1813023": {
@@ -108,7 +108,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1813023",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093826840.jpg?v=1303499728"
     },
     "1913002": {
@@ -116,7 +116,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1913002",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093745276.jpg?v=521139655"
     },
     "1913006": {
@@ -124,7 +124,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1913006",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093839170.jpg?v=169227319"
     },
     "1913007": {
@@ -132,7 +132,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "1913007",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093841986.jpg?v=1685620140"
     },
     "2013002": {
@@ -140,7 +140,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2013002",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093703167.jpg?v=2096669599"
     },
     "2013005": {
@@ -148,7 +148,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2013005",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093710138.jpg?v=2020523774"
     },
     "2013008": {
@@ -156,7 +156,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2013008",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093712514.jpg?v=1322782936"
     },
     "2013010": {
@@ -164,7 +164,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2013010",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093715137.jpg?v=971526352"
     },
     "2313001": {
@@ -172,7 +172,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2313001",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093716884.jpg?v=1567514669"
     },
     "2113002": {
@@ -180,7 +180,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2113002",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093719482.jpg?v=1040359213"
     },
     "2213002": {
@@ -188,7 +188,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213002",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093733811.jpg?v=1240540938"
     },
     "2213005": {
@@ -196,7 +196,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213005",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093736046.jpg?v=602188390"
     },
     "2213006": {
@@ -204,7 +204,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213006",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093738287.jpg?v=346109782"
     },
     "2213007": {
@@ -212,7 +212,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213007",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093740586.jpg?v=286294178"
     },
     "2213008": {
@@ -220,7 +220,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213008",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093743248.jpg?v=1948518521"
     },
     "2113004": {
@@ -228,7 +228,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2113004",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178086346210.jpg?v=516599445"
     },
     "2113006": {
@@ -236,7 +236,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2113006",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093747183.jpg?v=1593452227"
     },
     "2213009": {
@@ -244,7 +244,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213009",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093822877.jpg?v=1920180947"
     },
     "2113003": {
@@ -252,7 +252,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2113003",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178093750446.jpg?v=1328539613"
     },
     "2213003": {
@@ -260,7 +260,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2213003",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178094049770.jpg?v=2086932414"
     },
     "2313003": {
@@ -268,7 +268,7 @@ var examinersData = {
         "profession": "朗诵",
         "idNumber": "2313003",
         "employer": "江苏省朗诵协会",
-        "validUntil": "2029年7月",
+        "validUntil": "2029年12月",
         "photoUrl": "http://www.lsxh.zhaokaoyun.com//avatar/company/21/LS178094078255.jpg?v=1445963467"
     }
 };
